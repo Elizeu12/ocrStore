02f964121cd7ddaa50aef57b30d08cbeb77ee6d1
elizeu12 <elizeu.souza@bandtec.com.br>
2023-02-12 16:55:55 -0300

    melhorando leitura de preços

--- compras/app/src/main/java/com/belcompany/compras/CameraActionsFragment.kt
+++ compras/app/src/main/java/com/belcompany/compras/CameraActionsFragment.kt
@@ -1,17 +1,19 @@
 package com.belcompany.compras
 
 import android.annotation.SuppressLint
+import android.hardware.Camera.open
 import android.os.Bundle
+import android.system.Os.open
 import android.text.InputType
 import android.util.Log
 import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.camera.core.Camera
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.belcompany.compras.data.Element
 import com.google.android.gms.vision.CameraSource
@@ -23,14 +25,21 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 class CameraActionsFragment : Fragment(R.layout.camera_actions_fragment) {
 
     private var scan = false
-    private var total = 0.0
+    private var focus = false
     private lateinit var model: ViewModelStore
+    private val regexMoneyNoSignal = Regex("\\d{1,3}(\\.\\d{3})*(,\\d{2})?")
+    private val regexMoneySignalReal = Regex("\\d{1,3}(\\.\\d{3})*(.\\d{2})?")
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         view.findViewById<Button>(R.id.btn_scan).setOnClickListener {
             scan = true
+        }
+        view.findViewById<Button>(R.id.btn_focus).setOnClickListener {
+            focus = true
         }
 
         val textRecognizer = TextRecognizer.Builder(context).build()
@@ -55,59 +64,30 @@ class CameraActionsFragment : Fragment(R.layout.camera_actions_fragment) {
                         return
                     }
 
-//                    findViewById<TextView>(R.id).post {
-//                        val stringBuilder = StringBuilder()
-//                        for (i in 0 until items.size()) {
-//                            val item = items.valueAt(i)
-//                            stringBuilder.append(item.value)
-//                            stringBuilder.append("\n")
-//                        }
                     if (scan) {
+
+                        scan = false
                         val stringBuilder = StringBuilder()
                         for (i in 0 until items.size()) {
                             val item = items.valueAt(i)
                             stringBuilder.append(item.value)
                         }
 
-                        var textStringBuilder = stringBuilder.toString().lowercase()
+                        var textStringBuilder = stringBuilder.toString().uppercase()
+                        var textPrice = 0.00
 
-                        var monetary = ""
+                        val getprice = textStringBuilder.replace(" ", "")
 
-                        if (textStringBuilder.indexOf("r$") > 0) {
-                            monetary = "r$"
-                        } else {
-                            monetary = "rs"
+                        var matches = regexMoneyNoSignal.find(getprice)?.value
+
+                        if (matches == null){
+                            matches = regexMoneySignalReal.find(getprice)?.value
                         }
 
-                        val textPriceNotTreated = textStringBuilder.substring(
-                            textStringBuilder.indexOf(monetary),
-                            textStringBuilder.length
-                        )
-                        var textPrice = ""
-                        var count = false
-                        var valid = 0
-
-                        textPriceNotTreated.forEach {
-                            if (it.isDigit() && valid <= 1) {
-                                textPrice += it
-                                if (count) {
-                                    valid++
-                                }
-                            }
-                            if (it == ',' || it == '.') {
-                                textPrice += it
-                                count = true
-                            }
+                        val price = matches?.replace(',', '.')
+                        if (price != null) {
+                            textPrice = price.toDouble()
                         }
-
-                        count = false
-                        valid = 0
-
-                        textStringBuilder = textStringBuilder.replaceRange(
-                            textStringBuilder.indexOf(monetary),
-                            textStringBuilder.length,
-                            ""
-                        )
 
                         requireActivity().runOnUiThread {
 
@@ -122,23 +102,17 @@ class CameraActionsFragment : Fragment(R.layout.camera_actions_fragment) {
                                 }
                                 .setPositiveButton(resources.getString(R.string.text_confirm)) { _, _ ->
 
-                                    var valuePrice =
-                                        textPrice.replace(',', '.').toDouble()
-
                                     var value =
-                                        popupEditText.text.toString().toDouble() * valuePrice
+                                        popupEditText.text.toString().toDouble() * textPrice
 
-                                    total += value
-
-
-                                    model = ViewModelProvider(requireActivity())[ViewModelStore::class.java]
+                                    model =
+                                        ViewModelProvider(requireActivity())[ViewModelStore::class.java]
 
                                     val element = Element(textStringBuilder, value)
                                     model.updateData(element)
                                 }
                                 .show()
                         }
-                        scan = false
                     }
                 }
             })
@@ -154,7 +128,7 @@ class CameraActionsFragment : Fragment(R.layout.camera_actions_fragment) {
                 .setFacing(CameraSource.CAMERA_FACING_BACK)
                 .setRequestedPreviewSize(1280, 1024)
                 .setAutoFocusEnabled(true)
-                .setRequestedFps(2.0f)
+                .setRequestedFps(3.0f)
                 .build()
 
             cameraContainer.holder.addCallback(object : SurfaceHolder.Callback {
@@ -170,6 +144,7 @@ class CameraActionsFragment : Fragment(R.layout.camera_actions_fragment) {
                     try {
                         cameraProvider.unbindAll()
                         preview.start(cameraContainer.holder)
+
                     } catch (e: Exception) {
                         Log.e("logCamera", "Failure initialize camera", e)
                     }
--- compras/app/src/main/java/com/belcompany/compras/RecyclerListFragment.kt
+++ compras/app/src/main/java/com/belcompany/compras/RecyclerListFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
